c4c4528e917d493ccaa98aba819e928e48ccbe74
setiamuktiazizah <setiamuktiazizah3@student.uns.ac.id>
2023-12-18 19:28:20 +0700

    fix ui dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,13 +10,13 @@
     <title>Dashboard Admin</title>
 </head>
 <body>
-    <div class="content-wrapper">
+    <div class="content-wrapper" style="background-color: #293677">
         <!-- Content Header (Page header) -->
         <div class="content-header">
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h1 class="m-0">Dashboard</h1>
+                <h1 class="m-0" style="color:white">Dashboard</h1>
               </div><!-- /.col -->
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
