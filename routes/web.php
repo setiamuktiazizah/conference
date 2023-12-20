8bed698db287761b92ca34c6747e5f8817617539
e9739b <pevantiavania@gmail.com>
2023-12-20 20:40:23 +0700

    add payment status

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,7 @@ Route::get('/submituploadreviewmanuscript', function () {
 Route::get('/konfigurasipembayaran', function () {
     return view('bendahara.konfigurasi');
 });
+
+Route::get('/paymentstatus', function () {
+    return view('bendahara.paymentstatus');
+});
