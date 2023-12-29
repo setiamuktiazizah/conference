5f8006f34e1817f6bb7d5772a73dba30c28f16bb
GalihAdhi <galihadhi755@gmail.com>
2023-12-30 00:43:39 +0700

    fix logo admin-template

--- resources/views/template/admin-template.blade.php
+++ resources/views/template/admin-template.blade.php
@@ -180,7 +180,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-      <img src="images\Webdemia Logo.png" alt="Webdemia" style="width: 174px; height: 60px">
+      <img src="/images/Webdemia Logo.png" alt="Webdemia" style="width: 174px; height: 60px">
       {{-- <span class="brand-text font-weight-light">AdminLTE 3</span> --}}
     </a>
 
@@ -189,7 +189,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
